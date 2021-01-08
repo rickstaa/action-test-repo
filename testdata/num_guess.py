c0fd9f15202b95eba0aa03ef09fc45efb6fc3c9c
rickstaa <rick.staa@outlook.com>
2021-01-08 23:49:34 +0100

    Adds flake8 warning fail-on-error test

--- testdata/num_guess.py
+++ testdata/num_guess.py
@@ -1,10 +1,10 @@
 """Small test script taken from https://wiki.python.org/moin/SimplePrograms"""
 
 import random
-import sys # F401 'os' imported but unused
-import os # F401 'os' imported but unused
+import sys  # F401 'os' imported but unused
+import os  # F401 'os' imported but unused
 
-### E265 block comment should start with '# '
+# E265 block comment should start with '# '
 print("Hello from reviewdog!")
 print("Let's play a small number guessing game to test the flake8 github action.")
 print("This game is taken from https://wiki.python.org/moin/SimplePrograms.")
@@ -14,7 +14,9 @@ guesses_made = 0
 name = input("Hello! What is your name?\n")
 
 number = random.randint(1, 20)
-print("Well, {0}, I am thinking of a number between 1 and 20.".format(name)) # E501 line too long (80 > 79 characters)
+print(
+    "Well, {0}, I am thinking of a number between 1 and 20.".format(name)
+)  # E501 line too long (80 > 79 characters)
 
 while guesses_made < 6:
 
@@ -39,5 +41,3 @@ if guess == number:
     )
 else:
     print("Nope. The number I was thinking of was {0}".format(number))
-
-import itertools # E402 module level import not at top of file
